9311dc3a41cd02c3d4219d9afd0961b1ea483df7
TanguyWYK <tanguywest47@yahoo.fr>
2022-06-27 15:51:32 +0200

    Ajout polluants

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,5 @@
 'use strict';
 
+initPanel([],[1,1,1,2,3],'panel1');
+initPanel([],[2,2,2],'panel2');
+console.log(POLLUTANTS);
--- public/js/panel.js
+++ public/js/panel.js
@@ -1,11 +1,16 @@
 'use strict';
 
-let panel = new Vue({
-    el: '#panel',
-    data: {
-        measures: [1, 2, 3],
-    },
-    template: `<section>
+function initPanel(operatorButtons,measures,idElement){
+
+    let panel = new Vue({
+        el: '#'+idElement,
+        data: {
+            measures: measures,
+            counter: 0,
+            lastCounter: 10,
+            operatorButtons: operatorButtons,
+        },
+        template: `<section>
                     <table>
                       <tr>
                         <th>Mesure</th>
@@ -13,13 +18,28 @@ let panel = new Vue({
                       <tr v-for="measure in measures">
                         <td>{{ measure }}</td>
                       </tr>
+                      <tr><td>{{ counter }}</td></tr>
+                      <tr><td>{{ sum }}</td></tr>
                     </table>
                     <button v-on:click="changeMeasures">Clic</button>
                 </section>`,
-    computed: {},
-    methods: {
-        changeMeasures(){
-            Vue.set(this.measures,1,this.measures[1]+1);
-        }
-    },
-});
+        computed: {
+            sum: function () {
+                return this.counter + this.lastCounter;
+            }
+        },
+        methods: {
+            changeMeasures() {
+                this.counter++;
+                //Vue.set(this.measures,1,this.measures[1]+1);
+            }
+        },
+    });
+
+
+
+
+
+}
+
+
