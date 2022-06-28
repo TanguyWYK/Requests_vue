ae2d510e420b576c4f6774c71a03007525ac467a
Bilalgngt <bilal.gngt@outlook.com>
2022-06-28 14:45:49 +0200

    debut tableau

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
-
-initPanel([],[1,1,1,2,3],'panel1');
-initPanel([],[2,2,2],'panel2');
-console.log(POLLUTANTS);
+let pollutantsName = [];
+for(let pollutant of POLLUTANTS){
+    pollutantsName.push(pollutant.name_fr)
+}
+initPanel("Type polluant", ["="],[1,1,1,2,3],["Sol brut", "Sol eluat", "eau", "air"],'panel1');
+initPanel("Nom polluant", ["=", "≠"],[1,1,1,2,3],pollutantsName,'panel2');
+//initPanel([],[2,2,2],'panel2');
+//console.log(POLLUTANTS);
--- public/js/panel.js
+++ public/js/panel.js
@@ -1,25 +1,42 @@
 'use strict';
 
-function initPanel(operatorButtons,measures,idElement){
+function initPanel(tableName, operatorButtons,measures, selectOptions, idElement){
 
     let panel = new Vue({
         el: '#'+idElement,
         data: {
+            tableName: tableName,
             measures: measures,
             counter: 0,
             lastCounter: 10,
             operatorButtons: operatorButtons,
+            isActive: false,
+            selectOptions: selectOptions,
+            operator: null,
         },
         template: `<section>
                     <table>
-                      <tr>
-                        <th>Mesure</th>
-                      </tr>
-                      <tr v-for="measure in measures">
-                        <td>{{ measure }}</td>
-                      </tr>
-                      <tr><td>{{ counter }}</td></tr>
-                      <tr><td>{{ sum }}</td></tr>
+                        <thead>
+                        <tr>
+                            <th colspan="2">{{ tableName }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                        <td id="checkboxes" colspan="2"><input type="checkbox" checked></td>
+                        </tr>
+                        <tr >
+                            <td v-for="(operatorButton, index) in operatorButtons" class="operatorClass" v-on:click="selectedOperator($event)"  v-bind:id="'operator'+index">{{ operatorButton }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <select class="form-control">
+                                    <option v-for="selectOption in selectOptions" >{{ selectOption }}</option>
+                                </select>
+                            </td>
+                        </tr>
+                        </tbody>
                     </table>
                     <button v-on:click="changeMeasures">Clic</button>
                 </section>`,
@@ -32,6 +49,15 @@ function initPanel(operatorButtons,measures,idElement){
             changeMeasures() {
                 this.counter++;
                 //Vue.set(this.measures,1,this.measures[1]+1);
+            },
+            selectedOperator(event){
+                //this.isActive = !this.isActive;
+                console.log(event.target);
+                console.log(event.target.parentNode.childNodes);
+                event.target.parentNode.childNodes.forEach(element => element.classList.remove("selected"));
+                event.target.classList.toggle("selected");
+                this.operator = event.target.innerText;
+                console.log(this.operator);
             }
         },
     });
